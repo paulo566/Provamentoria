7a87037e3adf3a179818a5622820a900b8b6b89e
paulo566 <paulohenriquedossantoss057@gmail.com>
2025-07-09 14:15:55 -0300

    Add files via upload

---
+++ conversoTemperatura.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class conversoTemperatura {
+    public static void main(String[]args){
+        Scanner temp = new Scanner(System.in);
+        
+        double temperatura, kelvin, Fahrenheit;
+ System.out.println("conversor de Temperatura");
+        
+        System.out.println("Digite
+        
+}
